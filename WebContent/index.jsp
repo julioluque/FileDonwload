37f072c271419eaeebff3d184a6e47f2cc51aae9
Julio Luque <julio.a.luque@gmail.com>
2020-04-09 13:37:36 -0300

    Fix for download. Know you can download image, movie and document

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,9 +1,9 @@
 <%@taglib uri="http://struts.apache.org/tags-html" prefix="html" %>
 <pre>
 
-<html:link action="file.do?filenmae=strutsbook">DOWNLOAD STRUTS BOOK</html:link>
+<html:link action="file.do?filename=strutsbook">DOWNLOAD STRUTS BOOK</html:link>
 
-<%-- <html:link action="file.do?filenmae=pkmovie">DOWNLOAD PK MOVIE</html:link> --%>
+<html:link action="file.do?filename=pkmovie">DOWNLOAD PK MOVIE</html:link>
 
-<html:link action="file.do?filenmae=jlimage">JULIO IMAGES</html:link>
+<html:link action="file.do?filename=jlimage">JULIO IMAGES</html:link>
 </pre>
--- src/controller/FileDownloadController.java
+++ src/controller/FileDownloadController.java
@@ -18,6 +18,9 @@ public class FileDownloadController extends DownloadAction {
 		String filename = request.getParameter("filename");
 		
 		StreamInfo info = null;
+		if(filename== null) {
+			System.out.println("fail...");
+		}
 		
 		if(filename.contains("struts")) {
 			File f = new File("C:/Users/Alfredo/wkStruts/Struts librerias-recursos/EXTERNOS/FILEFORDOWNLOAD/struts1.docx");
@@ -25,12 +28,12 @@ public class FileDownloadController extends DownloadAction {
 			response.setContentType("application/msword");
 			response.setHeader("Content-Disposition", "attachment;filename=juliostrutsbook.docx");
 		}
-//		if(filename.contains("pk")) {
-//			File f = new File("C:/Users/Alfredo/wkStruts/Struts librerias-recursos/EXTERNOS/FILEFORDOWNLOAD/pk.3gp");
-//			info = new FileStreamInfo("video/x-msvideo", f);
-//			response.setContentType("video/x-msvideo");
-//			response.setHeader("Content-Disposition", "attachment;filename=juliostrutsvideo.3gp");
-//		}
+		if(filename.contains("pk")) {
+			File f = new File("C:/Users/Alfredo/wkStruts/Struts librerias-recursos/EXTERNOS/FILEFORDOWNLOAD/pk.3gp");
+			info = new FileStreamInfo("video/x-msvideo", f);
+			response.setContentType("video/x-msvideo");
+			response.setHeader("Content-Disposition", "attachment;filename=juliostrutsvideo.3gp");
+		}
 		if(filename.contains("image")) {
 			File f = new File("C:/Users/Alfredo/wkStruts/Struts librerias-recursos/EXTERNOS/FILEFORDOWNLOAD/image.png");
 			info = new FileStreamInfo("image/jpeg", f); 
